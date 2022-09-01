d87933afd618773f2674c1a6262067700c4bbea5
Ariadne Conill <ariadne@dereferenced.org>
2022-09-01 19:49:49 +0000

    add types package

--- go.mod
+++ go.mod
@@ -3,7 +3,17 @@ module chainguard.dev/wolfi-secdb
 go 1.19
 
 require (
+	github.com/spf13/cobra v1.5.0
+	gitlab.alpinelinux.org/alpine/go v0.5.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	mvdan.cc/sh/v3 v3.5.1 // indirect
 )
---
+++ pkg/types/db.go
@@ -0,0 +1,73 @@
+// Copyright 2022 Chainguard, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"fmt"
+	"os"
+
+	"gitlab.alpinelinux.org/alpine/go/pkg/apkbuild"
+	"gopkg.in/yaml.v3"
+)
+
+// TODO(kaniini): Harmonize these types with alpine's secdb implementation
+// and place the implementation in alpine-go.
+type Package struct {
+	Name     string            `json:"name"`
+	Secfixes apkbuild.Secfixes `json:"secfixes"`
+}
+
+type PackageEntry struct {
+	Pkg Package `json:"pkg"`
+}
+
+type Database struct {
+	Apkurl    string         `json:"apkurl"`
+	Archs     []string       `json:"archs"`
+	Reponame  string         `json:"reponame"`
+	Urlprefix string         `json:"urlprefix"`
+	Packages  []PackageEntry `json:"packages"`
+}
+
+type MelangePackage struct {
+	Package struct {
+		Name    string `yaml:"name"`
+		Version string `yaml:"version"`
+		Epoch   int    `yaml:"epoch"`
+	} `yaml:"package"`
+	Secfixes apkbuild.Secfixes `yaml:"secfixes"`
+}
+
+// Identity returns the package identity triple as apk-tools expects
+// it to be, e.g. `[name]-[version]-r[epoch].`
+func (mp MelangePackage) Identity() string {
+	return fmt.Sprintf("%s-%s-r%d", mp.Package.Name, mp.Package.Version, mp.Package.Epoch)
+}
+
+// LoadMelangePackage loads a Melange source package YAML file
+// and extracts the Secfixes data from it.
+func LoadMelangePackage(fileName string) (*MelangePackage, error) {
+	fileData, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	pkg := &MelangePackage{}
+	if err := yaml.Unmarshal(fileData, pkg); err != nil {
+		return nil, err
+	}
+
+	return pkg, nil
+}
