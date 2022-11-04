85b95287fd8c6603f59dc623e5c19f424874cd16
Ariadne Conill <ariadne@dereferenced.org>
2022-11-04 19:32:49 +0000

    add support for melange templates

--- go.mod
+++ go.mod
@@ -3,17 +3,26 @@ module chainguard.dev/wolfi-secdb
 go 1.19
 
 require (
+	github.com/Masterminds/sprig/v3 v3.2.2
 	github.com/spf13/cobra v1.5.0
-	gitlab.alpinelinux.org/alpine/go v0.5.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/huandu/xstrings v1.3.1 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	mvdan.cc/sh/v3 v3.5.1 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- pkg/types/db.go
+++ pkg/types/db.go
@@ -15,10 +15,22 @@
 package types
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"strings"
+	"text/template"
 
+	"github.com/Masterminds/sprig/v3"
 	"gopkg.in/yaml.v3"
+)
+
+const (
+	substitutionPackageName    = "${{package.name}}"
+	substitutionPackageVersion = "${{package.version}}"
+	substitutionPackageEpoch   = "${{package.epoch}}"
+	substitutionTargetsDestdir = "${{targets.destdir}}"
+	substitutionSubPkgDir      = "${{targets.subpkgdir}}"
 )
 
 // TODO(kaniini): Harmonize these types with alpine's secdb implementation
@@ -66,6 +78,47 @@ func (mp MelangePackage) Entry() PackageEntry {
 	}
 }
 
+func substitutionReplacements() map[string]string {
+	return map[string]string{
+		substitutionPackageName:    "MELANGE_TEMP_REPLACEMENT_PACAKAGE_NAME",
+		substitutionPackageVersion: "MELANGE_TEMP_REPLACEMENT_PACAKAGE_VERSION",
+		substitutionPackageEpoch:   "MELANGE_TEMP_REPLACEMENT_PACAKAGE_EPOCH",
+		substitutionTargetsDestdir: "MELANGE_TEMP_REPLACEMENT_DESTDIR",
+		substitutionSubPkgDir:      "MELANGE_TEMP_REPLACEMENT_SUBPKGDIR",
+	}
+}
+
+func applyTemplate(contents []byte) ([]byte, error) {
+	// First, replace all protected pipeline templated vars temporarily
+	// So that we can apply the Go template
+	// We have to do this bc go templates doesn't support ignoring certain fields: https://github.com/golang/go/issues/31147
+
+	sr := substitutionReplacements()
+
+	protected := string(contents)
+	for k, v := range sr {
+		protected = strings.ReplaceAll(protected, k, v)
+	}
+
+	tmpl, err := template.New("").Funcs(sprig.TxtFuncMap()).Parse(protected)
+	if err != nil {
+		return nil, err
+	}
+	tmpl = tmpl.Option("missingkey=error")
+	buf := bytes.NewBuffer([]byte{})
+	if err := tmpl.Execute(buf, nil); err != nil {
+		return nil, err
+	}
+
+	// Add the pipeline templating back in
+	templateApplied := buf.String()
+	for k, v := range sr {
+		templateApplied = strings.ReplaceAll(templateApplied, v, k)
+	}
+
+	return []byte(templateApplied), nil
+}
+
 // LoadMelangePackage loads a Melange source package YAML file
 // and extracts the Secfixes data from it.
 func LoadMelangePackage(fileName string) (*MelangePackage, error) {
@@ -74,8 +127,13 @@ func LoadMelangePackage(fileName string) (*MelangePackage, error) {
 		return nil, err
 	}
 
+	tmplData, err := applyTemplate(fileData)
+	if err != nil {
+		return nil, err
+	}
+
 	pkg := &MelangePackage{}
-	if err := yaml.Unmarshal(fileData, pkg); err != nil {
+	if err := yaml.Unmarshal(tmplData, pkg); err != nil {
 		return nil, err
 	}
 
